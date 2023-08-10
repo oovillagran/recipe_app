e1d13ebffe2f0dd79c7c5cde6f8faaeaf4a2d673
oovillagran <oscar.villagran@hotmail.com>
2023-08-09 20:19:09 -0500

    Add registration fomr styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable
+         :recoverable, :rememberable, :validatable
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
 
   has_many :recipes, foreign_key: 'user_id'
